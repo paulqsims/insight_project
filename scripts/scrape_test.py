6dcb2cd5ae947e2f8856cee52c7558b7295db029
Paul Sims <paulqsims@users.noreply.github.com>
2020-09-21 21:00:21 -0400

    Add selenium library and import chrome driver

--- scripts/scrape_test.py
+++ scripts/scrape_test.py
@@ -2,8 +2,14 @@
 Module for scraping skincare product data from the web. 
 """
 
-import requests
+#import requests
 
-URL = 'https://www.paulaschoice.com/ingredient-dictionary'
-page = requests.get(URL)
+#URL = 'https://www.paulaschoice.com/ingredient-dictionary'
+#page = requests.get(URL)
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+
+# access chrome driver
+driver = webdriver.Chrome('~/Paul/Downloads/chromedriver')
