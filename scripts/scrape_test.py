19c4b7300d543233ad1fc6226c71517585b2c8b8
Paul Sims <paulqsims@users.noreply.github.com>
2020-09-21 23:51:52 -0400

    Correct ingredient name for loop and add  description

--- scripts/scrape_test.py
+++ scripts/scrape_test.py
@@ -22,11 +22,17 @@ driver = webdriver.Chrome(f"{path}/chromedriver")
 # Access website
 driver.get('https://www.paulaschoice.com/ingredient-dictionary')
 
-ingredient_names = driver.find_elements_by_xpath('//p[@class="description ingredient-description"]')
+ingredient_names = driver.find_elements_by_xpath('//h2[@class="name ingredient-name"]')
 
 ingredients_list = []
 for ingredient in range(len(ingredient_names)):
     ingredients_list.append(ingredient_names[ingredient].text)
+
+ingredient_descript = driver.find_elements_by_xpath('//p[@class="description ingredient-description"]')
+
+ingredient_descript_list = []
+for description in range(len(ingredient_descript)):
+    ingredient_descript_list.append(ingredient_descript[description].text)
 
 ingredient_rating = driver.find_elements_by_xpath('//td[@class="col-rating ingredient-rating rating-good"]')
 
@@ -40,11 +46,11 @@ ingredient_categories = []
 for category in range(len(ingredient_cat)):
     ingredient_categories.append(ingredient_cat[category].text)
 
-df = pd.DataFrame(columns=['Ingredient','Rating','Category']) # creates master dataframe 
+df = pd.DataFrame(columns=['Ingredient','Description','Rating','Category']) # creates master dataframe 
 
-data_tuples = list(zip(ingredients_list[1:],ingredient_ratings[1:],
+data_tuples = list(zip(ingredients_list[1:],ingredient_descript_list[1:],ingredient_ratings[1:],
 ingredient_categories[1:])) # list of each ingredient with ratings and categories paired
-temp_df = pd.DataFrame(data_tuples, columns=['Ingredient','Rating','Category']) # creates dataframe of each tuple in list
+temp_df = pd.DataFrame(data_tuples, columns=['Ingredient', 'Description','Rating','Category']) # creates dataframe of each tuple in list
 df = df.append(temp_df) # appends to master dataframe
 
 driver.close()
