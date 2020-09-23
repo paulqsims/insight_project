89165a4bd9b87c2d8ab6ecc2ce8e01ec7b99999d
Paul Sims <paulqsims@users.noreply.github.com>
2020-09-23 10:57:56 -0400

    Append ingredients back to empty list

--- scripts/scrape_ratings.py
+++ scripts/scrape_ratings.py
@@ -151,6 +151,7 @@ for link in product_links:
         ingredient_element = driver.find_element_by_css_selector("div.ingredients").get_attribute("textContent")
     except NoSuchElementException: 
         ingredient_element = math.nan
+    item_ingredients.append(ingredient_element)
     # CHANGE ratings-stars number for each rating score!!!!
     rating_element = driver.find_element_by_xpath("//span[@class='product-rating rating stars-3']").get_attribute("outerHTML")
     item_rating.append(rating_element)
@@ -168,6 +169,7 @@ df.to_csv(f"{path}/data/rating3_scrape.csv")
 
 #### Access website for ratings of 4 ####
 
+driver = webdriver.Chrome(f"{path}/chromedriver")
 driver.get(ratings_webpages[3])
 
 # Extract html link references 
@@ -196,6 +198,7 @@ for link in product_links:
         ingredient_element = driver.find_element_by_css_selector("div.ingredients").get_attribute("textContent")
     except NoSuchElementException: 
         ingredient_element = math.nan
+    item_ingredients.append(ingredient_element)
     # CHANGE ratings-stars number for each rating score!!!!
     rating_element = driver.find_element_by_xpath("//span[@class='product-rating rating stars-4']").get_attribute("outerHTML")
     item_rating.append(rating_element)
@@ -241,6 +244,51 @@ for link in product_links:
         ingredient_element = driver.find_element_by_css_selector("div.ingredients").get_attribute("textContent")
     except NoSuchElementException: 
         ingredient_element = math.nan
+    
+driver = webdriver.Chrome(f"{path}/chromedriver")
+driver.get(ratings_webpages[3])
+
+# Extract html link references 
+time.sleep(5)
+item_list = driver.find_elements_by_xpath("//div[@class='review-result']//div[@class='review-details']//div[@class='review-col col-2']//a[starts-with(@href, 'https://www.beautypedia.com/products/')]")
+
+# Convert selenium ref info to href links and store them in a vector
+product_links = []
+for i, link in enumerate(item_list):
+    # print(link.get_attribute('href'))
+    # Fetch and store the links
+    product_links.append(link.get_attribute('href'))
+
+# Create empty lists to store results in 
+item_names = []
+item_ingredients = []
+item_rating = []
+
+# Iterate over links to extract text data
+for link in product_links:
+    time.sleep(5)
+    driver.get(link)
+    item_name_element = driver.find_elements_by_xpath("//h1[@class='product-name']")[0].text
+    item_names.append(item_name_element)
+    try:
+        ingredient_element = driver.find_element_by_css_selector("div.ingredients").get_attribute("textContent")
+    except NoSuchElementException: 
+        ingredient_element = math.nan
+    item_ingredients.append(ingredient_element)
+    # CHANGE ratings-stars number for each rating score!!!!
+    rating_element = driver.find_element_by_xpath("//span[@class='product-rating rating stars-4']").get_attribute("outerHTML")
+    item_rating.append(rating_element)
+
+# Combine lists into dataframe and export as CSV for pandas processing
+df = pd.DataFrame(columns=['Product','Ingredients','Rating']) # creates master dataframe 
+
+data_tuples = list(zip(item_names[1:],item_ingredients[1:],item_rating[1:])) # list of each ingredient with ratings and categories paired
+temp_df = pd.DataFrame(data_tuples, columns=['Product', 'Ingredients','Rating']) # creates dataframe of each tuple in list
+df = df.append(temp_df)
+
+driver.close()
+
+df.to_csv(f"{path}/data/rating4_scrape.csv")
     # CHANGE ratings-stars number for each rating score!!!!
     rating_element = driver.find_element_by_xpath("//span[@class='product-rating rating stars-5']").get_attribute("outerHTML")
     item_rating.append(rating_element)
